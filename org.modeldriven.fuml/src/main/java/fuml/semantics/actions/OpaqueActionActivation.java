f2bee40e6ebb74019d61ba8071b06d0b33dc065e
Zsolt <zsolt.sandor@gmail.com>
2021-09-18 19:13:14 +0200

    First working version of opaque action

--- org.modeldriven.fuml/src/main/java/fuml/semantics/actions/OpaqueActionActivation.java
+++ org.modeldriven.fuml/src/main/java/fuml/semantics/actions/OpaqueActionActivation.java
@@ -1,13 +1,31 @@
 package fuml.semantics.actions;
 
+import fuml.semantics.simpleclassifiers.IntegerValue;
 import fuml.syntax.actions.OpaqueAction;
+import fuml.syntax.actions.OutputPin;
 
 public class OpaqueActionActivation extends ActionActivation {
 
+	public OpaqueActionActivation() {
+		super();
+	}
+	
 	@Override
 	public void doAction() {
 		OpaqueAction action = (OpaqueAction) this.node;
+
 		System.out.println("Running an opaque action: " + action);
+		
+		for (OutputPin pin : action.output) {
+			
+			System.out.println("Sending output token int = 5 to pin : " + pin.name);
+			
+			IntegerValue value = new IntegerValue();
+			value.value = 5;
+			
+			this.putToken(pin, value);
+		}
+		
 	}
 
 }
--- org.modeldriven.fuml/src/main/java/fuml/syntax/actions/OpaqueAction.java
+++ org.modeldriven.fuml/src/main/java/fuml/syntax/actions/OpaqueAction.java
@@ -2,4 +2,19 @@ package fuml.syntax.actions;
 
 public class OpaqueAction extends fuml.syntax.actions.Action{
 
+	public UMLPrimitiveTypes.StringList body = new UMLPrimitiveTypes.StringList();
+	public UMLPrimitiveTypes.StringList language = new UMLPrimitiveTypes.StringList();
+
+	public OpaqueAction() {
+		super();
+	}
+	
+	public void addBody(String body) {
+		this.body.add(body);
+	} // addBody
+
+	public void addLanguage(String language) {
+		this.language.add(language);
+	} // addLanguage
+	
 }
